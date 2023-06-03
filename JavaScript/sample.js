d221e2e694af689eefba5521c6becd425ccd8c26
Cashadi <ichascashadi@gmail.com>
2023-06-04 00:29:03 +0700

    study kasus

---
+++ JavaScript/sample.js
@@ -0,0 +1,9 @@
+for (let i = 0; i < 15; i++) {
+  if (i / 5 == 1) {
+    console.log('ini mah angka 5');
+  } else if (i == 11) {
+    console.log('ini mah angka 11');
+  } else {
+    console.log('ini mah bukan angka 5');
+  }
+}
